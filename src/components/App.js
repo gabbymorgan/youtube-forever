16f431230fe5beae18df06870b8fe775619b32bd
William Pelton <williamrpelton@gmail.com>
2018-04-30 00:36:57 -0500

    added social links and fixed footer position

--- src/components/App.js
+++ src/components/App.js
@@ -13,8 +13,11 @@ class App extends Component {
         src="https://www.youtube.com/embed/videoseries?list=PLLNToNPi40EgcXJjnrmAtFWEIAWftZGWr&autoplay=1&controls=0&loop=1&rel=0&showinfo=0&autohide=1&wmode=transparent&hd=1">
         </iframe>
       </div>
-      <header className="Footer">
-      </header>
+        <footer className="Footer">
+          <a href='https://www.facebook.com/Tubular-Music-2094381257512633/'><img src={require('../fb.png')} id="fb"/></a>
+          <a href='https://twitter.com/TubularMusic'><img src={require('../twitter.png')} id="twitter"/></a>
+          <a href='https://www.instagram.com/tubular80s/'><img src={require('../ig.png')} id="ig"/></a>
+        </footer>
       </div>
     );
   }
