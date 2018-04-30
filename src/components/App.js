64e70908971f11ba148faf51164d316920e12295
William Pelton <williamrpelton@gmail.com>
2018-04-29 23:42:21 -0500

    header, footer, and logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Img from 'react-image';
 import './App.css';
 
 class App extends Component {
@@ -7,15 +6,15 @@ class App extends Component {
     return (
       <div>
         <header className="Header">
-          <Img src={require("../logo.png")} className="Header__Img"/>
+          <img src={require("../logo.png")} className="Header__Img"/>
         </header>
       <div className='Container'>
         <iframe title="video" id='Video'
         src="https://www.youtube.com/embed/videoseries?list=PLLNToNPi40EgcXJjnrmAtFWEIAWftZGWr&autoplay=1&controls=0&loop=1&rel=0&showinfo=0&autohide=1&wmode=transparent&hd=1">
         </iframe>
       </div>
-      <footer className="Footer">
-      </footer>
+      <header className="Footer">
+      </header>
       </div>
     );
   }
